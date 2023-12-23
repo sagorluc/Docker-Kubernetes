0e5b076b99cd00d1af8b5d8bb9bef0226bef6ec2
sagorluc <mdsagorluc@gmail.com>
2023-12-23 12:08:52 +0600

    Docker python

---
+++ Docker/simple_python/multiple.py
@@ -0,0 +1,13 @@
+
+# def mul(m):
+#     return m * 2
+
+# input_val = int(input("Enter a number : "))   
+# res = mul(input_val)
+# print(f'Multiple of {input_val} by 2 { res}')
+
+idx = 1
+
+while idx < 6: 
+    print('Docker test ',idx)
+    idx += 1
